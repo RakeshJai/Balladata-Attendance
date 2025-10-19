8014e7419f5fba7f7b38c68a8358c9e9ca3d7c8a
RakeshJai <rakeshjai0369@gmail.com>
2025-10-19 13:21:27 -0500

    PWA Functionallity #1

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'attendance-pwa-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './Logo.png',
+  './Logo.png',
+  'https://code.getmdl.io/1.3.0/material.indigo-pink.min.css',
+  'https://code.getmdl.io/1.3.0/material.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
